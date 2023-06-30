82ced5ee9a4c085b69071014e1ef99a8d2013c50
hyukjin <cdh5771@naver.com>
2023-06-30 17:55:09 +0900

    chore: test

--- src/main/java/com/sparta/studycommunity/StudyCommunityApplication.java
+++ src/main/java/com/sparta/studycommunity/StudyCommunityApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StudyCommunityApplication {
-
+    // git test
     public static void main(String[] args) {
         SpringApplication.run(StudyCommunityApplication.class, args);
     }
