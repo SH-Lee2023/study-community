fcdab263b091311ac3316fe2d30e34a2eb45c966
Eunbi Kim <eunbkim4@gmail.com>
2023-07-04 15:21:04 +0900

    fix: build.gradle 복구

--- src/main/java/com/sparta/studycommunity/StudyCommunityApplication.java
+++ src/main/java/com/sparta/studycommunity/StudyCommunityApplication.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 @SpringBootApplication
 public class StudyCommunityApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(StudyCommunityApplication.class, args);
     }
